4d459d573ff33b865fc912a41336e5a2283a2d31
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-05-17 18:57:25 +0530

    learned function call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [LoginComponent, ProfileComponent]
 })
 export class AppComponent {
   title = 'learn-angular-youtube';
+  x = 10;
+  y = 20;
+  user1 = "Saketh";
+  user2 = "Sampath";
+  user3 = "Vinay"
+  handleClickEvent() {
+    console.log("Handle click event successfully");
+    this.otherFunction()
+  }
+
+  otherFunction() {
+    console.log("Other Function Called");
+  }
 }
+
