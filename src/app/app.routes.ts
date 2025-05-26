1ae9ee6270b9e116ce739cbe6d9c9274843c0c95
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-05-26 11:10:30 +0530

    pagenotfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LoginsComponent } from './logins/logins.component';
 import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path: "about", component: AboutComponent},
     {path: "login", component: LoginsComponent},
     {path: "contact", component: ContactComponent},
+    {path: "**", component: PageNotFoundComponent}
 ];
