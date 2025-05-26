1f1afa1fecc17b4d717fb23b6150c6ab75306327
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-05-26 14:50:12 +0530

    pass data from one page to another page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { AboutComponent } from './about/about.component';
 import { LoginsComponent } from './logins/logins.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    {path: "", component: HomeComponent},
     {path: "about", component: AboutComponent},
     {path: "login", component: LoginsComponent},
     {path: "contact", component: ContactComponent},
+    {path: "profile", component: ProfileComponent, data: {name: "John Doe"}},
     {path: "**", component: PageNotFoundComponent}
 ];
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,19 @@
-import { Component } from "@angular/core"
-
+import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
-    selector: "app-profile",
-    // template: `<h1>Profile Component</h1>`,
-    // styles: `h1{color: green}`,
-    templateUrl: "./profile.component.html",
-    styleUrl: "./profile.component.css",
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
 })
+export class ProfileComponent {
+  username: string | null = '';
 
-export class ProfileComponent {}
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    this.route.data.subscribe((data) => {
+        this.username = data['name'];
+    })
+  }
+}
