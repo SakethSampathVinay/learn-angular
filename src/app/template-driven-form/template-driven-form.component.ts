94401b64fa1ba6d184ebb4d6c0c99713488a8cc1
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-05-27 14:21:49 +0530

    learned template-driven-form-validations

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { FormsModule, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.css'
 })
