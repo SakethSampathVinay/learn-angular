5bb9b6bd22257467a1b231dec096d0a5c5e121fa
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-05-27 11:38:57 +0530

    learned form validation

--- src/app/form-grouping/form-grouping.component.ts
+++ src/app/form-grouping/form-grouping.component.ts
@@ -1,17 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-grouping',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './form-grouping.component.html',
   styleUrl: './form-grouping.component.css',
 })
 export class FormGroupingComponent {
   profileForm = new FormGroup({
-    name: new FormControl('saketh'),
-    password: new FormControl('123'),
-    email: new FormControl('saketh@test.com'),
+    name: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.minLength(8)]),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(50), Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]),
   });
 
   onSubmit() {
@@ -24,5 +25,17 @@ export class FormGroupingComponent {
       password: '456',
       email: 'sakethsampath2006@gmail.com',
     });
+  }
+
+  get name() {
+    return this.profileForm.get('name');
+  }
+
+  get email() {
+    return this.profileForm.get('email');
+  }
+
+  get password() {
+    return this.profileForm.get('password');
   }
 }
